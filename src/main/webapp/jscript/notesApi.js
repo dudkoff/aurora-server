5ff7057f815afa1c39adc51ca6d9b77a85c09726
Artem Dudkov <dudkoff@Artems-Mac-mini.local>
2016-10-03 17:10:48 -0400

    base point. Wrong type, but nothing else broken

--- src/main/webapp/jscript/notesApi.js
+++ src/main/webapp/jscript/notesApi.js
@@ -1,28 +1,11 @@
-$("#refresh").click(
-    $( document ).ready(function() {
-        var xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState == XMLHttpRequest.DONE) {
-                var response = xhr.responseText.substring(1, xhr.responseText.length-1);
-                response = response.split("},");
-                var  notes = "";
-                for (i in response) {
-                    if (response[i][response[i].length-1] != "}") {
-                        var note = JSON.parse(response[i] + "}");
-                    } else {
-                        var note = JSON.parse(response[i]);
-                    }
-                    notes += note.author + ":    " + note.text + "\n____________________________________________________________\n\n";
-                }
-                $("#resultBox").text(notes);
-            }
-        }
-        xhr.open('GET', 'http://www.artemdudkov.com/aurora/notes/all', true);
-        xhr.send(null);
-    })
-);
+$("#refresh").click(function() {
+    location.reload();
+    $( document ).ready(function(){
+        console.log("reloaded");
+    })});
 
 $("#addNotes").click(function() {
+    alert("addNote pressed!")
     var xhr = new XMLHttpRequest();
     var text = "";
     var author = "";
@@ -36,7 +19,8 @@ $("#addNotes").click(function() {
     } else if (author == ""){
         alert("No author. Please sign your note.");
     } else {
-        xhr.open('POST', 'http://www.artemdudkov.com/aurora/notes/post', true);
+        // xhr.open('POST', 'http://localhost:8080/notes/post', true);
+        xhr.open('POST', 'http://www.artemdudkov.com:8080/aurora/notes/post', true);
         xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
         xhr.send(JSON.stringify({text:text, author:author}));
         location.reload();
