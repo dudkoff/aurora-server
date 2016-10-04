43284a6404b447e5e986c3d5e0a5006276cc4ce8
dudkoff <dudkoff@umich.edu>
2016-10-04 14:27:25 -0400

    IT WORLKSgit add .! apart form minor bugs :)

--- src/main/webapp/jscript/notesApi.js
+++ src/main/webapp/jscript/notesApi.js
@@ -1,11 +1,12 @@
-$("#refresh").click(function() {
+$("#refresh").click(function reloadPage() {
     location.reload();
+    console.log("page reloaded");
     $( document ).ready(function(){
         console.log("reloaded");
     })});
 
 $("#addNotes").click(function() {
-    alert("addNote pressed!")
+    console.log("attempting to adda note");
     var xhr = new XMLHttpRequest();
     var text = "";
     var author = "";
@@ -23,6 +24,33 @@ $("#addNotes").click(function() {
         xhr.open('POST', 'http://www.artemdudkov.com:8080/aurora/notes/post', true);
         xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
         xhr.send(JSON.stringify({text:text, author:author}));
-        location.reload();
+        console.log("note added");
+        updateResultBox();
+        $("#newNote").val("");
+        console.log("result is UPDATED");
     }
 });
+
+function updateResultBox() {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            var response = xhr.responseText.substring(1, xhr.responseText.length-1);
+            response = response.split("},");
+            var  notes = "";
+            for (i in response) {
+                if (response[i][response[i].length-1] != "}") {
+                    var note = JSON.parse(response[i] + "}");
+                } else {
+                    var note = JSON.parse(response[i]);
+                }
+                notes += note.author + ":    " + note.text + "\n____________________________________________________________\n\n";
+            }
+            $("#resultBox").text(notes);
+        }
+    }
+    // xhr.open('GET', 'http://localhost:8080/notes/all', true);
+    xhr.open('GET', 'http://www.artemdudkov.com:8080/aurora/notes/all', true);
+    xhr.send(null);
+    console.log("resultbox is SET");
+}
