c9ef5ff9d6154200e75a4ca7f5824a81be7143a3
dudkoff <dudkoff@umich.edu>
2016-10-06 15:45:11 -0400

    Good design progress

---
+++ src/main/webapp/jscript/notesPrompts.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Dudkoff on 9/29/16.
+ */
+$(".notes").load("html/notes.html");
--- src/main/webapp/jscript/vigenerePrompts.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by Dudkoff on 9/29/16.
- */
-$(".vigenere").load("html/vigenere.html");
